b316d1423fe760bf1326143f7116ac386eaf52ec
Ivan Scherbakov <ivashka1113@mail.ru>
2022-11-04 18:59:49 +0700

    Выполнил усложненное задание 26 урока

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 "use strict";
 
+let user;
+
 const sendData = (res) => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(res)
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+    console.log(res)
+    xhr.send(JSON.stringify(
+        res
+    ));
+    xhr.onload = () => console.log(xhr.response);
 }
 
-const getData = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            sendData(data);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+const getData = (func) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "db.json");
+    xhr.send();
+    xhr.onload = () => {
+        user = xhr.response;
+        func(user);
+    }
 }
 
-getData('db.json', sendData)
+getData(sendData)
